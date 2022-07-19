cbe8dc55328ceee694b3b8d04fbdd3aeb5bfa062
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-07-19 19:35:24 +0530

    Add files via upload

--- colorizer.py
+++ colorizer.py
@@ -5,13 +5,12 @@ from deoldify import device
 from deoldify.device_id import DeviceId
 from deoldify.visualize import get_image_colorizer
 
-torch.hub.set_dir('models')
-warnings.filterwarnings('ignore')
-torch.backends.cudnn.benchmark = True
-device.set(device=DeviceId.CPU) # GPU0
-colorizer = get_image_colorizer(artistic=False)
-
 def colorize(filename,output):
+    torch.hub.set_dir('models')
+    warnings.filterwarnings('ignore')
+    torch.backends.cudnn.benchmark = True
+    device.set(device=DeviceId.CPU) # GPU0
+    colorizer = get_image_colorizer(artistic=False)
     colorizer.get_transformed_image(filename, render_factor=35, watermarked=False).save(output)
     
    
